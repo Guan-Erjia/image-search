bfec090490c37eb80dfff5ddc1796d6b87dcdd64
guanjianzhou <guanjianzhou@baidu.com>
2024-10-15 14:00:22 +0800

    1323

--- app/image/route.ts
+++ app/image/route.ts
@@ -1,10 +1,16 @@
-import { readFileSync } from "fs";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
+  const query = request.nextUrl.searchParams.get("q");
   const response = await fetch(
-    "https://www.google.com.hk/search?q=%E5%90%8E%E5%AE%A4+level+50&udm=2"
+    `https://www.google.com.hk/search?q=${query}&udm=2`
   );
   const text = await response.text();
-  return new Response(text);
+  const imageList = text.match(/<img[^>]*>/g);
+  const srcList = imageList?.map((img) => img.match(/src="([^"]+)"/)?.[1]);
+  return new NextResponse(JSON.stringify(srcList.slice(1)), {
+    headers: {
+      "content-type": "application/json",
+    },
+  });
 }
