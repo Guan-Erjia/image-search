11d7cefb34f88738700187fe8421c9578a5ca365
guanjianzhou <guanjianzhou@baidu.com>
2024-10-15 14:19:41 +0800

    23423423

--- app/route.ts
+++ app/route.ts
@@ -2,15 +2,19 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
   const query = request.nextUrl.searchParams.get("q");
-  const response = await fetch(
-    `https://www.google.com.hk/search?q=${query}&udm=2`
-  );
-  const text = await response.text();
-  const imageList = text.match(/<img[^>]*>/g);
-  const srcList = imageList?.map((img) => img.match(/src="([^"]+)"/)?.[1]);
-  return new NextResponse(JSON.stringify(srcList.slice(1)), {
-    headers: {
-      "content-type": "application/json",
-    },
-  });
+  try {
+    const response = await fetch(
+      `https://www.google.com.hk/search?q=${query}&udm=2`
+    );
+    const text = await response.text();
+    const imageList = text.match(/<img[^>]*>/g);
+    const srcList = imageList?.map((img) => img.match(/src="([^"]+)"/)?.[1]);
+    return new NextResponse(JSON.stringify(srcList.slice(1)), {
+      headers: {
+        "content-type": "application/json",
+      },
+    });
+  } catch (error) {
+    return new NextResponse(JSON.stringify([]));
+  }
 }
